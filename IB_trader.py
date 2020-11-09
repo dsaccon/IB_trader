f2eb79f268deac96d123c3aa1fe313869783a78e
ds <deerff@gmail.com>
2020-11-09 14:35:05 -0800

    fix period boundary calc bug in _check_period(), add flag arg to _calc_new_candle() to switch ha calc methods

--- IB_trader.py
+++ IB_trader.py
@@ -266,7 +266,7 @@ class MarketDataApp(EClient, EWrapper):
     def _check_period(self):
         # Return True if period ends at this update, else False
         _time = dt.datetime.fromtimestamp(self._tohlc[0])
-        total_secs = _time.hour*60 + _time.minute*60 + _time.second
+        total_secs = _time.hour*3600 + _time.minute*60 + _time.second
         if total_secs % self.period == 0:
             return True
         return False
@@ -297,7 +297,7 @@ class MarketDataApp(EClient, EWrapper):
             if self.candles.shape[0] > 2:
                 bar_color_prev = self.candles['ha_color'].values[-2].upper()
             logging.info('--')
-            logging.warning(f'Candle: {self.args.symbol} - {bar_color}, Prev: {bar_color_prev}')
+            logging.warning(f'Candle: {self.cache[-1][0]}, {self.args.symbol} - {bar_color}, Prev: {bar_color_prev}')
             csv_row = [col[1] for col in _pd.items()]
             csv_row.insert(1, self.args.symbol)
             self._write_csv_row((csv_row,), self.logfile_candles)
@@ -307,7 +307,7 @@ class MarketDataApp(EClient, EWrapper):
         else:
             raise ValueError
 
-    def _calc_new_candle(self):
+    def _calc_new_candle(self, use_prev_ha=False):
         ohlc = (
             self.cache[0][1],
             max([u[2] for u in self.cache]),
@@ -317,13 +317,14 @@ class MarketDataApp(EClient, EWrapper):
         if self.candles.shape[0] > 0:
             # Can only calc heikin-ashi if we have previous data
             ha_c = (ohlc[0] + ohlc[1] + ohlc[2] + ohlc[3])/4
-#            if self.candles.shape[0] == 1:
-#                # First HA candle
-#                #ha_o = (ohlc[0] + ohlc[3])/2
-#                #ha_o = (self.candles['open'].values[-1] + self.candles['close'].values[-1])/2
-#            else:
-#                ha_o = (self.candles['ha_open'].values[-1] + self.candles['ha_close'].values[-1])/2
-            ha_o = (self.candles['open'].values[-1] + self.candles['close'].values[-1])/2
+            if use_prev_ha:
+                if self.candles.shape[0] == 1:
+                    # No prior HA candle is available, use prev raw candle open/close
+                    ha_o = (self.candles['open'].values[-1] + self.candles['close'].values[-1])/2
+                else:
+                    ha_o = (self.candles['ha_open'].values[-1] + self.candles['ha_close'].values[-1])/2
+            else:
+                ha_o = (self.candles['open'].values[-1] + self.candles['close'].values[-1])/2
             ha_h = max(ohlc[1], ha_o, ha_c)
             ha_l = min(ohlc[2], ha_o, ha_c)
             ha_color = 'Red' if self.candles['ha_close'].values[-1] > ha_c else 'Green'
@@ -344,9 +345,9 @@ class MarketDataApp(EClient, EWrapper):
         }
         return _pd
 
-    def _cache_update(self, ohlc):
+    def _cache_update(self, tohlc):
         # Still in the middle of a period. Cache data for processing at end of period
-        self.cache.append(ohlc)
+        self.cache.append(tohlc)
 
     def _check_order_conditions(self):
         if not isinstance(self.candles['ha_color'].values[-1], str):
